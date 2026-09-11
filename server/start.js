8ab2729c7888c14576ac9a2b649cd2de94a5c7f0
s1307110taivstpedutw-star <s1307110.taivs.tp.edu.tw@gmail.com>
2026-09-11 14:56:42 +0800

    Create start.js

---
+++ server/start.js
@@ -0,0 +1,9 @@
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    "Storage System modular server running on port " + PORT
+  );
+});
