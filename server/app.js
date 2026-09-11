fc81195fb93c6db1e70c569bc75d0703cb7cb305
s1307110taivstpedutw-star <s1307110.taivs.tp.edu.tw@gmail.com>
2026-09-11 14:12:34 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,22 +2,53 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 
+const auth = require("./routes/auth");
+
 const app = express();
+
+
+// ==============================
+// 基本 Middleware
+// ==============================
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// ==============================
+// Session
+// ==============================
+
 app.use(
   session({
-    secret: "arm_key_secret_123",
+    secret: process.env.SESSION_SECRET || "arm_key_secret_123",
     resave: false,
     saveUninitialized: false,
+
     cookie: {
-      secure: false
+      secure: false,
+      maxAge: 8 * 60 * 60 * 1000
     }
   })
 );
 
+
+// ==============================
+// 靜態網站
+// ==============================
+
 app.use(express.static(path.join(__dirname, "..", "public")));
+
+
+// ==============================
+// 登入系統 API
+// ==============================
+
+app.use("/api", auth.router);
+
+
+// ==============================
+// 匯出 App
+// ==============================
 
 module.exports = app;
